35869568a8c2bbaf16d8fee314be9a35b3af31c1
getvibe <tsartors31@ccp-stl.org>
2026-09-12 08:09:30 -0500

    done6

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig =
-  apiKey: "AIzaSyBwvxO2FJIiSGll_4xdXjv_UIKK4U02-Zc",
-  authDomain: "gamble-io.firebaseapp.com",
-  projectId: "gamble-io",
-  storageBucket: "gamble-io.firebasestorage.app",
-  messagingSenderId: "191214216573",
-  appId: "1:191214216573:web:63f6f15a8987061da99814",
-  measurementId: "G-JYPZD5S9FS"
-};
